7957220719bd5df023d2fefd2349c9fadd0f48d2
Ivan Yaremko <ivan.yaremko@gmail.com>
2024-11-23 14:35:00 +0000

    commit

--- sql/queries/saved_posts.sql
+++ sql/queries/saved_posts.sql
@@ -13,3 +13,9 @@ created_at
 
 -- name: GetSavedPosts :many
 SELECT * FROM saved_posts ORDER BY created_at DESC;
+
+-- name: GetSavedPost :one
+SELECT * FROM saved_posts WHERE url = ?;
+
+-- name: DeleteSavePost :exec
+DELETE FROM saved_posts WHERE url = ?;
