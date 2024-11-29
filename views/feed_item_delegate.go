a52d757c5962e6098eef8c9a189cb0d43ad516e0
IvanYaremko <ivan.yaremko@gmail.com>
2024-11-29 12:33:47 +0000

    commit

--- views/feed_item_delegate.go
+++ views/feed_item_delegate.go
@@ -1,9 +1,6 @@
 package views
 
 import (
-	"context"
-	"time"
-
 	"github.com/IvanYaremko/rssdukester/sql/database"
 	"github.com/charmbracelet/bubbles/key"
 	"github.com/charmbracelet/bubbles/list"
@@ -33,29 +30,10 @@ func feedItemDelegate(q *database.Queries, rss item) list.DefaultDelegate {
 			switch {
 			case key.Matches(msg, enterBinding):
 				return transitionView(item)
-
-			case key.Matches(msg, saveBinding):
-				return savePostItem(q, item, rss.title)
 			}
 		}
 		return nil
 	}
 
 	return d
-}
-
-func savePostItem(q *database.Queries, selected item, feed string) tea.Cmd {
-	return func() tea.Msg {
-		params := database.SavePostParams{
-			Url:       selected.url,
-			Title:     selected.title,
-			Feed:      feed,
-			CreatedAt: time.Now(),
-		}
-		err := q.SavePost(context.Background(), params)
-		if err != nil {
-			return fail{}
-		}
-		return success{}
-	}
 }
