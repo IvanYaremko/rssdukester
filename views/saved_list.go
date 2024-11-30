0429371afe85061b14f17d846229db858ac5052f
IvanYaremko <ivan.yaremko@gmail.com>
2024-11-30 20:59:33 +0000

    commit

--- views/saved_list.go
+++ views/saved_list.go
@@ -63,13 +63,13 @@ func (s *saved) getSavedPosts() tea.Msg {
 	items := make([]list.Item, 0, len(saved))
 	for _, post := range saved {
 		hyperlink := fmt.Sprintf("\x1b]8;;%s\x07%s\x1b]8;;\x07", post.Url, "Article link →")
-
-		savedDate := subtleStyle.Italic(true).Render(post.SavedAt.Format(formateDate))
+		pubDate := attentionStyle.Italic(true).Render(post.PublishedAt.Format(formateDate))
+		//savedDate := subtleStyle.Italic(true).Render(post.SavedAt.Format(formateDate))
 
 		desc := fmt.Sprintf("%s • %s • %s",
 			specialStyle.Render(hyperlink),
 			attentionStyle.Render(post.FeedName),
-			savedDate,
+			pubDate,
 		)
 
 		items = append(items, item{
