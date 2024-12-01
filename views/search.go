e3fe8eea61e257bd3e859b129b4dac7462865c34
IvanYaremko <ivan.yaremko@gmail.com>
2024-12-01 15:29:26 +0000

    commit

--- views/search.go
+++ views/search.go
@@ -2,6 +2,7 @@ package views
 
 import (
 	"github.com/IvanYaremko/rssdukester/sql/database"
+	"github.com/charmbracelet/bubbles/key"
 	"github.com/charmbracelet/bubbles/list"
 	"github.com/charmbracelet/bubbles/spinner"
 	"github.com/charmbracelet/bubbles/textinput"
@@ -16,6 +17,35 @@ type search struct {
 	searchInput textinput.Model
 	err         error
 	searchTerm  string
+}
+
+func initialiseSearch(q *database.Queries) search {
+	s := spinner.New()
+	s.Spinner = spinner.Dot
+	s.Style = highlightStyle
+
+	items := make([]list.Item, 0)
+	l := list.New(items, feedItemDelegate(), width, height)
+	l.Title = "SEARCH RESULTS"
+	l.Styles.Title = highlightStyle
+	l.AdditionalShortHelpKeys = func() []key.Binding {
+		return []key.Binding{
+			enterBinding,
+		}
+	}
+
+	ti := textinput.New()
+	ti.Placeholder = "Enter search terms..."
+	ti.Focus()
+	ti.Width = width
+
+	return search{
+		quries:      q,
+		spinner:     s,
+		loading:     true,
+		list:        l,
+		searchInput: ti,
+	}
 }
 
 func (s search) Init() tea.Cmd {
